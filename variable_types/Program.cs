63702b49c7b563eefd5be1d6325706804a63db81
Samet Candan <c._samet@hotmail.com>
2023-08-13 13:43:35 +0300

    first commit

---
+++ variable_types/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int sayi=10;
+int sayi2=20;
+long sayi3= 50;
+short sayi4= 5;
+byte sayi5=10;
+
+float a= 10.5f;
+double b= 20.6;
+decimal c= 100.56m;
+
+string name= "samet candan";
+
+char ch= "a";
+
+bool isRetired= true;
+
+bool isActive= false;
+
+Console.WriteLine(sayi);
+Console.WriteLine(sayi2);
+
+
+
