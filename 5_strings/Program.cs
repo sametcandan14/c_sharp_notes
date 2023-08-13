9f8ad2fa68be24fb322b765f881867401b78f359
Samet Candan <c._samet@hotmail.com>
2023-08-13 15:28:51 +0300

    strings

---
+++ 5_strings/Program.cs
@@ -0,0 +1,16 @@
+﻿
+Console.Write("Name: ");
+string name =Console.ReadLine();
+Console.Write("Surname: ");
+string surname =Console.ReadLine();
+
+
+Console.Write("Age: ");
+int age = int.Parse(Console.ReadLine());
+
+
+// string str= "My name is "+ name+ " " +surname+ "and I'm " + age+ " years old.";
+// string str= $"My name is {name} {surname} and I'm {age} years old.";
+string str = string.Format("My name is {0} {1} and I'm {2} years old", name, surname, age);
+
+System.Console.WriteLine(str);
